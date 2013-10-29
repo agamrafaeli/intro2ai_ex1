d2709003f231ec683405af4452e4d020967be72b
Agam Rafaeli <agam.rafaeli@gmail.com>
2013-10-29 20:34:21 +0200

    new BFS + new corner eater

--- search.py
+++ search.py
@@ -129,21 +129,35 @@ def breadthFirstSearch(problem):
     start = problem.getStartState()
     beenThere = []
     
-    fringe.push( (start,[]) )
-    
+#     fringe.push( (start,[]) )    
+#     while not fringe.isEmpty():
+#         (node,nodePath) = fringe.pop()
+#         nextSteps = problem.getSuccessors(node)
+#         for step in nextSteps:
+#             newPath = list(nodePath)
+#             newPath.append(step[1])
+#             
+#             if problem.isGoalState(step[0]):
+#                 print newPath
+#                 return newPath
+#     
+#             if step not in beenThere:
+#                 beenThere.append(step)
+#                 fringe.push( (step[0],newPath) )
+
+    fringe.push( (start,[],[False,False,False,False]) )    
     while not fringe.isEmpty():
-        (node,nodePath) = fringe.pop()
-        nextSteps = problem.getSuccessors(node)
+        state = fringe.pop()
+        nextSteps = problem.getSuccessors(state)
         for step in nextSteps:
-            newPath = list(nodePath)
-            newPath.append(step[1])
             
-            if problem.isGoalState(step[0]):
-                return newPath
+            if problem.isGoalState(step):
+                print step[1]
+                return step[1]
     
             if step not in beenThere:
                 beenThere.append(step)
-                fringe.push( (step[0],newPath) )
+                fringe.push( step )
     return []
     
   
